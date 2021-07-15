71da75b12fc8ec18b352a4de52868ebb87941371
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-07-15 13:09:55 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
@@ -14,39 +13,92 @@ console.log('Importing module');
 // import add,  { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
 
-import add, {cart} from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('milk', 10);
 
 console.log(cart);
-*/
 
 const shoppingCart2 = (function () {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
 
-    const addToCart = function (product, quantity) {
-        cart.push({ product, quantity });
-       console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
-    };
-    
-    const orderStock = function (product, quantity) {
-        cart.push({ product, quantity });
-       console.log(`${quantity} ${product} ordered from supplier`);
-    };
-    
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity,
-    };
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
 })();
 
 shoppingCart2.addToCart('apple', 4);
 shoppingCart2.addToCart('pizza', 2);
 console.log(shoppingCart2);
 console.log(shoppingCart2.shippingCost);
+
+// Export
+// export.addTocart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+// };
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
+
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const kai = new Person('Kai');
+
+console.log('Kai' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
